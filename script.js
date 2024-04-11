53a82d4ca475637d3c4360043ebb6d7ace188331
OneMight <dimamirankov63@gmail.com>
2024-04-11 21:27:28 +0300

    feat: start work with sixes block. trying to make night theme

--- script.js
+++ script.js
@@ -2,28 +2,46 @@ const buttonswitch = document.getElementById("check");
 let box = document.getElementsByClassName('box');
 let text = document.getElementsByTagName('p');
 let header = document.querySelector('header');
+let logo = document.getElementById('logo');
+let singup = document.getElementById('Sign-up');
+let login = document.getElementById('Log-in');
+
 let username;
 let password;
 
 function backgroundswitch(){
     if(buttonswitch.checked){
-        document.body.style.backgroundColor = '#011242';
-        header.style.backgroundColor = '#011242';
+       let a = document.querySelectorAll('nav > a');
+       document.body.classList.add("night-theme");
+       header.classList.add("night-theme");
+        logo.style.content = "url(./images/logowhite.png)";
+        singup.classList.add("night-theme");
+        login.classList.add("night-theme");
         for (let elem of text) {
-            elem.style.color = "#ffffff";
+           elem.classList.add("night-text");
+           a.classList.add("night-text");
         }
         for(let elem of box){
-            elem.style.backgroundColor = "#011242";
+            
+            elem.classList.add("night-theme");
         }
-    } else {
-        header.style.backgroundColor = '#FCF8F3';
-        document.body.style.backgroundColor = "";
+       
+    }else {
+        document.body.classList.remove("night-theme");
+        header.classList.remove("night-theme");
+        logo.style.content = "url(./images/logo.svg)";
+        singup.style.color = "#000000";
+        singup.style.backgroundColor = "#FCF8F3";
+        login.style.color = "#000000";
+        login.style.backgroundColor = "#FCF8F3";
+     
         for(let elem of box){
-            elem.style.backgroundColor = "";
+            elem.classList.remove("night-theme");
         }
         for (let elem of text) {
-            elem.style.color = ""; 
+            elem.classList.remove("night-text");
         }
+        
     }
 }
 
